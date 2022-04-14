f6add411d91c6ffa53526de88d923c84ae929256
Verity Carlos <verity.carlos@gmail.com>
2022-04-14 14:33:54 -0500

    add info about breweries

--- script.js
+++ script.js
@@ -24,3 +24,24 @@ const attachClicksToLinks = () =>{
         brewery.addEventListener('click', displayBrewery)
     })
 }
+
+const displayBrewery = (e) => {
+    console.log(e.target.dataset.id)
+    const info = document.getElementById('info')
+    const ul = document.getElementById("breweries-list")
+    ul.innerHTML = ''
+    fetch(BASE_URL + `/breweries/${e.target.dataset.id}`)
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data)
+        info.innerHTML = `<h1>${data.name}</h1>
+        <h3>City:</h3>
+        <p>${data.city}</p>
+        <h3>Type:</h3>
+        <p>${data.brewery_type}</p>
+        <h3>Phone:</h3>
+        <p>${data.phone}</p>
+        <h3>URL</h3>
+        <p>${data.website_url}</p>`
+    })
+}
