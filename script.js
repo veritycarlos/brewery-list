1bc8d05cd62cf1a4100c6432efc359377cdd01b8
Verity Carlos <verity.carlos@gmail.com>
2022-04-22 15:05:41 -0500

    added state from api to brewery list

--- script.js
+++ script.js
@@ -63,6 +63,7 @@ const displayBrewery = (e) => {
         info.innerHTML = 
         `<h2>${data.name}</h2>
         <h3>State:</h3>
+        <p>${data.state}</p>
         <h3>City:</h3>
         <p>${data.city}</p>
         <h3>Type:</h3>
